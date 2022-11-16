fb4c1035fa0aba63e92b7b2882eb4e6748b1743e
rballage <rballage@student.42.fr>
2022-11-16 13:41:50 +0100

    added mkdir db_volume

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 // import { PrismaService } from './prisma/prisma.service';
 // import { UserService } from './user/user.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
 	providers: [],
 })
 export class AppModule {}
