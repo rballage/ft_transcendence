ec1d369158441f5c1a7db5aba2199063d822f3b3
rballage <rballage@student.42.fr>
2022-11-16 10:50:12 +0100

    new cleaner project structure

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+// import { PrismaService } from './prisma/prisma.service';
+// import { UserService } from './user/user.service';
+
+@Module({
+  imports: [],
+	providers: [],
+})
+export class AppModule {}
