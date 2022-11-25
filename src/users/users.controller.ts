99a84c90b8e5fcf7a24672bc14b7c4a203818453
rballage <rballage@student.42.fr>
2022-11-25 09:56:40 +0100

    clean deps & cleaner use of useguard in Users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,25 +4,22 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/users.dto';
 import { UserProfile} from './types/users.types';
 import { User } from '@prisma/client';
-import { LocalAuthGuard } from 'src/auth/guard/local-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
-	@UseGuards(JwtAuthGuard)
 	@Get(':username')
 	async getUser(@Param('username') username: string): Promise<User> {
 		console.log('getUser', username);
 		return await this.usersService.getUser(username);
 	}
-	@UseGuards(JwtAuthGuard)
 	@Post('')
 	async newUser(@Body() userDto: CreateUserDto): Promise<User> {
 		return await this.usersService.createUser(userDto);
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get(':username/profile')
 	async getProfile(@Param('username') username: string): Promise<UserProfile> {
 		console.log('getProfile', username);
