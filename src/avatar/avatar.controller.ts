15de05a5e09ba592a1465904dcefeae4206a0fa9
rballage <rballage@student.42.fr>
2022-12-05 21:25:26 +0100

    simple avatar upload working

--- src/avatar/avatar.controller.ts
+++ src/avatar/avatar.controller.ts
@@ -1,23 +1,22 @@
-import { Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, Req, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { diskStorage } from 'multer';
 import { IRequestWithUser } from 'src/auth/auths.interface';
 import JwtAuthGuard from 'src/auth/guard/jwt-auth.guard';
 import { AvatarService } from './avatar.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { saveAvatarToStorage } from './helpers/avatar-storage';
 
-@UseGuards(JwtAuthGuard)
+// @UseGuards(JwtAuthGuard)
 @Controller('avatar')
 export class AvatarController {
   constructor(private readonly avatarService: AvatarService) {}
 
-	@Post()
-	@UseInterceptors(FileInterceptor('file', {
-		storage: diskStorage({
-			destination: process.cwd() + 'avatars'
-		})
-	}))
-	uploadAvatar(@Req() request: IRequestWithUser, @UploadedFile() file: Express.Multer.File) {
-
+	@UseGuards(JwtAuthGuard)
+	@Post('upload')
+	@UseInterceptors(FileInterceptor('avatar', saveAvatarToStorage))
+	async uploadAvatar(@UploadedFile() avatar : Express.Multer.File, @Req() request: IRequestWithUser) {
+		// console.log(avatar);
 	}
 
 }
