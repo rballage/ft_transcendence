4ad1d5e7750d6ad202d42cf77a4a579dc03e915e
rballage <rballage@student.42.fr>
2022-12-06 16:18:17 +0100

    proper imports in avatar helper

--- src/avatar/helpers/avatar-storage.ts
+++ src/avatar/helpers/avatar-storage.ts
@@ -1,11 +1,13 @@
 import { diskStorage } from 'multer';
-import { Controller, HttpException, Post, Req, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
+// import { Controller, HttpException, Post, Req, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 
-import { IRequestWithUser } from 'src/auth/auths.interface';
-import {v4 as uuid } from 'uuid';
-const fs = require('fs')
+// import { IRequestWithUser } from 'src/auth/auths.interface';
+// import {v4 as uuid } from 'uuid';
+import * as fs from 'fs';
+// const fs = require('fs')
+import * as path from 'path';
 // const FileType = require('file-type')
-const path = require('path')
+// const path = require('path')
 
 type validFileExtension = 'png' | 'jpeg' | 'jpg';
 type validMimeType = 'image/png' | 'image/jpeg' | 'image/jpg';
@@ -18,7 +20,7 @@ export const saveAvatarToStorage = {
 		destination: './images',
 		filename: function (request: any, file, callback) {
 			console.log(file);
-			const fileExtension: validFileExtension = path.extname(file.originalname);
+			const fileExtension: string = path.extname(file.originalname);
 			const fileName: string = request?.user?.username + '.orginal' + fileExtension;
 			callback(null, fileName);
 		}
