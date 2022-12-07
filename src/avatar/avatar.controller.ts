0b08b22c0011caa1916944dc52399bed771747d5
rballage <rballage@student.42.fr>
2022-12-07 14:56:51 +0100

    working avatar upload, convert..., not fully tested yet

--- src/avatar/avatar.controller.ts
+++ src/avatar/avatar.controller.ts
@@ -6,21 +6,24 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { saveAvatarToStorage } from './helpers/avatar-storage';
 import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma.service';
 
 // @UseGuards(JwtAuthGuard)
 @Controller('avatar')
 export class AvatarController {
 	constructor(private readonly avatarService: AvatarService,
+				private readonly prismaService : PrismaService,
 				private readonly usersService : UsersService) {}
 
 	// @UseGuards(JwtAuthGuard)
-	@Post('upload')
+	@Post('')
 	@UseInterceptors(FileInterceptor('avatar', saveAvatarToStorage))
 	async uploadAvatar(@UploadedFile() avatar : Express.Multer.File, @Req() request: IRequestWithUser) {
 		if (request.fileValidationError) throw new BadRequestException(request.fileValidationError);
         else if (!avatar) throw new BadRequestException('invalid file');
 		const resFromDb = await this.usersService.addAvatar(undefined, avatar.path); // undefined for testing, change to username !
-		await this.avatarService.convertAvatar(avatar, resFromDb);
+		const ret = await this.avatarService.convertAvatar(avatar, resFromDb);
+		return await this.prismaService.avatar.update({where: { id : ret.id }, data: {...ret}})
 		//TODO
 		// save this file path to database as : user.avatar.linkOriginal / -> OK
 		// create new async process which will convert original file to jpg format if needed, 
@@ -28,7 +31,15 @@ export class AvatarController {
 		// 1. large: 500x500px
 		// 2. medium: 250x250px
 		// 3. thumbnail: 100x100px
-		return {message: 'file has been uploaded', filename: avatar.filename};
 	}
 	
 }
+			// return this.prismaService.avatar.update({
+			// 	where: { id : avatarDbEntry.id },
+			// 	data: {
+			// 		linkOriginal: OutputAvatarOriginalPath,
+			// 		linkLarge: OutputAvatarLargePath,
+			// 		linkMedium: OutputAvatarMediumPath,
+			// 		linkThumbnail: OutputAvatarthumbnailPath
+			// 	}, // sorry theo
+			// })
