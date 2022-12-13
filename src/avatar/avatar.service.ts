a8fc960132658946e004e003577b9dc428d8fb2e
rballage <rballage@student.42.fr>
2022-12-13 19:24:33 +0100

    avatar deletion

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -7,10 +7,12 @@ import { Avatar } from '@prisma/client';
 import { UserWhole } from 'src/users/types/users.types';
 import { Readable } from 'stream';
 import etag from 'etag';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AvatarService {
-	constructor(private readonly usersService:UsersService) {}
+	constructor(private readonly usersService:UsersService,
+		private readonly prismaService:PrismaService) {}
 
 	async getAvatar(username: string, size: string) : Promise<any>{
 		const user: UserWhole = await this.usersService.getWholeUser(username);
@@ -30,6 +32,26 @@ export class AvatarService {
 			stream: file,
 			filename: selectedAvatar,
 			tag: (user.avatars?.updatedAt ? String(new Date(user.avatars.updatedAt).getTime()) + selectedAvatar : selectedAvatar)};
+	}
+	async deleteAvatar(username: string) : Promise<any>{
+		const avatar: Avatar = await this.prismaService.avatar.findUnique({where : {username : username}});
+		if (!avatar || avatar.linkOriginal == '_default.original.webp')
+		    return true;
+		const avatarArr : string [] = [avatar.linkOriginal, avatar.linkLarge, avatar.linkMedium, avatar.linkThumbnail];
+		avatarArr.forEach((value : string) => {
+			console.log('Deleting: ', value)
+			try {
+				fs.unlinkSync(value);
+			} catch (error) {
+				console.log(error)
+			}
+		})
+		return await this.prismaService.avatar.update({where : {username : username},
+		    data : {
+                linkOriginal : '_default.original.webp',
+			    linkLarge : '_default.large.webp',
+			    linkMedium : '_default.medium.webp',
+			    linkThumbnail : '_default.thumbnail.webp'}});
 	}
 
 	async cropToSquareIfNecessary(originalFile : string) : Promise<any>
