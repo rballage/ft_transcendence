c7a478ba57743d77ee767bab8036ab1a28f542b6
Aymeric SINGEVIN <ssingevi@e1r8p14.clusters.42paris.fr>
2023-01-16 15:03:21 +0100

    minor change game

--- src/ws/game.service.ts
+++ src/ws/game.service.ts
@@ -6,7 +6,7 @@ export class Speed {
        this.x = 10;
        this.y = Math.random() * 5 
     }
-}
+};
 
 export class Ball {
     x : number;
@@ -20,116 +20,71 @@ export class Ball {
         this.speed = new Speed;
      }
 }
-
-export class Computer {
-    score: number;
-    y : number;
-    speedRatio: number;
-
-    constructor () {
-        this.score = 0;
-        this.y = 310;
-        this.speedRatio = 1
-    }
-};
-
 export class Player {
-    score: number;
-    y : number;
+    username    : string;
+    socketID    : string;
+    score       : number;
+    y           : number;
 
-    constructor () {
-        this.score = 0;
-        this.y = 310;
-    }
+    constructor (username: string, socketID: string) {
+        this.username   = username;
+        this.socketID   = socketID;
+        this.score      = 0;
+        this.y          = 310;
+    };
 };
 
 export class Game {
-    player: Player;
-    computer: Computer;
-    ball: Ball;
+    playerOne   : Player;
+    playerTwo   : Player;
+    ball        : Ball;
 
-    constructor () {
-        this.player = new Player;
-        this.computer = new Computer;
-        this.ball = new Ball;
+    constructor (usernameP1: string, socketIDP1: string, usernameP2: string, socketIDP2: string) {
+        this.playerOne  = new Player(usernameP1, socketIDP1);
+        this.playerTwo  = new Player(usernameP2, socketIDP2);
+        this.ball       = new Ball;
     }
 };
 
-export class GameInfo {
-    canvas: HTMLCanvasElement;
-    height_ratio : number;
-    width_ratio : number;
-    player_height : number;
-    player_width : number;
-    max_speed : number;
-    game : Game;
-    anim : number;
+export class GameInfo {   
+    player_height   : number;
+    player_width    : number;
+    max_speed       : number;
+    game            : Game;
+    anim            : number;
     
-    constructor() {
-        // this.canvas = <HTMLCanvasElement> document.getElementById('canvas');
-        this.height_ratio = 1
-        this.width_ratio = 1
+    constructor(usernameP1: string, socketIDP1: string, usernameP2: string, socketIDP2: string) {
         this.player_height = 100;
         this.player_width = 5;
         this.max_speed = 20;
-        this.game = new Game;
+        this.game = new Game(usernameP1, socketIDP1, usernameP2, socketIDP2);
         this.anim = 0;
-        }
-
-    // draw() {
-    //     // console.log("draw");
-    //     this.height_ratio = this.canvas.height / 720;
-    //     this.width_ratio = this.canvas.width / 1100;
-    //     var context = <CanvasRenderingContext2D> this.canvas.getContext('2d');
-    //     // this.getinfoserv();
-    //     // Draw field
-    //     context.fillStyle = 'black';
-    //     context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-    //     // Draw middle line
-    //     context.strokeStyle = 'white';
-    //     context.beginPath();
-    //     context.moveTo(this.canvas.width / 2, 0);
-    //     context.lineTo(this.canvas.width / 2, this.canvas.height);
-    //     context.stroke();
-
-    //     // Draw players
-    //     context.fillStyle = 'white';
-    //     context.fillRect(0, this.game.player.y * this.height_ratio, this.player_width * this.width_ratio, this.player_height * this.height_ratio);
-    //     context.fillRect(this.canvas.width - (this.player_width * this.width_ratio), this.game.computer.y * this.height_ratio , this.player_width * this.width_ratio, this.player_height * this.height_ratio);
-
-    //     // Draw ball
-    //     context.beginPath();
-    //     context.fillStyle = 'white';
-    //     context.arc(this.game.ball.x * this.width_ratio, this.game.ball.y * this.height_ratio, this.game.ball.r * this.height_ratio, 0, Math.PI * 2, false);
-    //     context.fill();
-    // };
+    };
     //server part
     changeDirection(playerPosition: any) {
         var impact = this.game.ball.y - playerPosition - this.player_height / 2;
         var ratio = 100 / (this.player_height / 2);
 
-        // Get a value between 0 and 10
+        // Get a value between -10 and 10
         this.game.ball.speed.y = Math.round(impact * ratio / 10);
-        console.log(this.game.ball.speed.y)
-    };
-    computerMove() {
-        this.game.computer.y += this.game.ball.speed.y * this.game.computer.speedRatio * 0.9;
+        // console.log(this.game.ball.speed.y)
     };
     collide(player: any) {
         
         if (this.game.ball.y < player.y || this.game.ball.y > player.y + this.player_height) {
             this.reset();
-            if (player == this.game.player) {
-                this.game.computer.score++;
+            if (player == this.game.playerOne) {
+                this.game.playerOne.score++;
+                this.resetScoreUpdate(1);
             } else {
-                this.game.player.score++;
+                this.game.playerTwo.score++;
+                this.resetScoreUpdate(-1);
             }
         } else {
             this.game.ball.speed.x *= -1;
             this.changeDirection(player.y);
             if (Math.abs(this.game.ball.speed.x) < this.max_speed) {
-                this.game.ball.speed.x *= 1.2;
+                this.game.ball.speed.x += 1;
             }
         }
     };
@@ -139,38 +94,66 @@ export class GameInfo {
         }
 
         if (this.game.ball.x > (1100 - this.player_width)) {
-            this.collide(this.game.computer);
+            this.collide(this.game.playerTwo);
         } else if (this.game.ball.x < this.player_width) {
-            this.collide(this.game.player);
+            this.collide(this.game.playerOne);
         }
 
-        // this.game.ball.x += this.game.ball.speed.x;
-        // this.game.ball.y += this.game.ball.speed.y;
+        this.game.ball.x += this.game.ball.speed.x;
+        this.game.ball.y += this.game.ball.speed.y;
     };
+    resetScoreUpdate(speedDirection : number)
+    {
+        this.game.ball.x = 550;
+        this.game.ball.y = 360;
+        this.game.playerOne.y = 310;
+        this.game.playerTwo.y = 310;
+        this.game.ball.speed.x = 10;
+        this.game.ball.speed.y = Math.random() * 3 * speedDirection;
+    }
     reset() {
         this.game.ball.x = 550;
         this.game.ball.y = 360;
-        this.game.player.y = 310;
-        this.game.computer.y = 310;
+        this.game.playerOne.y = 310;
+        this.game.playerTwo.y = 310;
         this.game.ball.speed.x = 10;
         this.game.ball.speed.y = Math.random() * 3;
     };
+    play() {
+        if (this.game.playerOne.score === 2 || this.game.playerTwo.score === 2) {
+            console.log("end game")
+            this.stop();
+            return;
+        }
+        this.checkDisconect();
+        this.ballMove();
+        // async ? this.getPlayersMoves();
+        /* emit data */
+        /* wait 1000/30 ms ? */
+        this.play();
+    };
+    getPlayersMoves()
+    {
+        /* receive playerOne mouseposition */
+        /* receive playerTwo mouseposition */
+    };
+    checkDisconect () {
+
+        if(this.game.playerOne /* is diconected */ )
+        {
+            this.game.playerTwo.score = 10;
+            this.game.playerOne.score = 0;
+            this.stop();
+        }
+        else if(this.game.playerTwo /* is diconected */ )
+        {
+            this.game.playerOne.score = 10;
+            this.game.playerTwo.score = 0;
+            this.stop();
+        }
+    };
+    stop ()
+    {
+        /* emit end of game */
+    };
 };
-
-/*
-play() {
-            if (this.gameInfo.game.computer.score === 2 || this.gameInfo.game.player.score === 2) {
-                console.log("end game")
-                this.stop();
-                return;
-            }
-
-            this.gameInfo.draw();
-            this.gameInfo.computerMove();
-            this.gameInfo.ballMove();
-
-
-
-            this.gameInfo.anim = requestAnimationFrame(this.play);
-        },
-*/
