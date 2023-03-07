1a8eef20effea5d755c80a16176fdaf112454fd3
rballage <rballage@student.42.fr>
2023-03-07 08:54:02 +0100

    prep mise en prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,22 @@ import { WsModule } from "./ws/ws.module";
 import { AuthModule } from "./auth/auth.module";
 import { ScheduleModule } from "@nestjs/schedule";
 import * as dotenv from "dotenv";
-import { AuthService } from "./auth/auth.service";
-import { JwtStrategy } from "./auth/strategy/jwt.strategy";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 dotenv.config();
 
 @Module({
-    imports: [ScheduleModule.forRoot(), AuthModule, WsModule, UsersModule, AvatarModule],
-
+    imports: [
+        ScheduleModule.forRoot(),
+        AuthModule,
+        WsModule,
+        UsersModule,
+        AvatarModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, "..", "client"),
+            exclude: ["/api*"],
+        }),
+    ],
     providers: [PrismaService],
     exports: [PrismaService, AuthModule, WsModule],
 })
