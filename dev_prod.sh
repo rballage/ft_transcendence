a788748659781f03f45f1dd8ad930b57d1d59001
rballage <rballage@student.42.fr>
2023-03-07 13:34:35 +0100

    added install script and fix 'npm run prod'

--- dev_prod.sh
+++ dev_prod.sh
@@ -2,6 +2,7 @@ mkdir -p ~/goinfre/db_volume;
 docker-compose -f ./docker-compose.yml up --build -d;
 sleep 2;
 prisma migrate dev -n update;
+prisma generate;
 cd client && npx quasar build && cd ..;
 
 nest start;
