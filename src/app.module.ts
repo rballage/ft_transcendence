fcfa0f63aa63180d2d3e06395cbc15279ff367f4
rballage <rballage@student.42.fr>
2023-03-20 11:29:16 +0100

    production prep: dynamic AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { UsersModule } from "./users/users.module";
 import { AvatarModule } from "./avatar/avatar.module";
@@ -11,18 +11,38 @@ import { join } from "path";
 dotenv.config();
 
 @Module({
-    imports: [
-        ScheduleModule.forRoot(),
-        AuthModule,
-        WsModule,
-        UsersModule,
-        AvatarModule,
-        // ServeStaticModule.forRoot({
-        //     rootPath: join(__dirname, "../..", "client", "dist"),
-        //     exclude: ["/api*"],
-        // }),
-    ],
-    providers: [PrismaService],
-    exports: [PrismaService, AuthModule, WsModule],
+    // imports: [
+    //     ScheduleModule.forRoot(),
+    //     AuthModule,
+    //     WsModule,
+    //     UsersModule,
+    //     AvatarModule,
+    //     // ServeStaticModule.forRoot({
+    //     //     rootPath: join(__dirname, "../..", "client", "dist"),
+    //     //     exclude: ["/api*"],
+    //     // }),
+    // ],
+    // providers: [PrismaService],
+    // exports: [PrismaService, AuthModule, WsModule],
 })
-export class AppModule {}
+export class AppModule {
+    static register(): DynamicModule {
+        const imports = [ScheduleModule.forRoot(), AuthModule, WsModule, UsersModule, AvatarModule];
+        const providers = [PrismaService];
+        const exports = [PrismaService, AuthModule, WsModule];
+        if (process.env.NODE_ENV === "prod") {
+            imports.push(
+                ServeStaticModule.forRoot({
+                    rootPath: join(__dirname, "../..", "client", "dist"),
+                    exclude: ["/api*"],
+                })
+            );
+        }
+        return {
+            module: AppModule,
+            imports,
+            providers,
+            exports,
+        };
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { WsSocketAdapter } from "./ws/ws.adapter";
-// import Joi from 'joi';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
+    const app = await NestFactory.create(AppModule.register(), {
         forceCloseConnections: true,
     });
     app.useWebSocketAdapter(new WsSocketAdapter(app));
