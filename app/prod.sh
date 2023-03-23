3b050459845b5ce17dea51a81174e7f1343c1847
rballage <rballage@student.42.fr>
2023-03-23 11:06:37 +0100

    dockerisation prep: changed repo structure

--- app/prod.sh
+++ app/prod.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/goinfre/db_volume;
-docker-compose -f ./docker-compose.yml up --build -d;
+docker-compose -f ../docker-compose.yml up --build -d;
 sleep 2;
 prisma migrate dev -n update;
 prisma generate;
