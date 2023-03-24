34f939bed00652b43b8f09aa5f55e22454d2c18c
rballage <rballage@student.42.fr>
2023-03-24 13:13:56 +0100

    dockerisation wip ok

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM node:18-alpine
 
-WORKDIR /app/src/
+FROM node:19-slim
+
+WORKDIR /app/
 
 COPY --chown=node:node app .
 
--- app/prod.sh
+++ app/prod.sh
@@ -1,5 +1,5 @@
-mkdir -p ~/goinfre/db_volume;
-docker-compose -f ../docker-compose.yml up --build -d;
+# mkdir -p ~/goinfre/db_volume;
+# docker-compose -f ./docker-compose.yml up --build -d;
 sleep 2;
 prisma migrate dev -n update;
 prisma generate;
