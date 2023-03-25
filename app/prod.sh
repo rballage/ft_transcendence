91531152103ac896dec3b66fd13dc9f07906c5d9
rballage <rballage@student.42.fr>
2023-03-25 12:58:14 +0100

    migration clean

--- app/prod.sh
+++ app/prod.sh
@@ -1,8 +1,8 @@
 # mkdir -p ~/goinfre/db_volume;
 # docker-compose -f ./docker-compose.yml up --build -d;
 sleep 2;
-prisma migrate dev -n update;
-prisma generate;
+npx prisma migrate deploy;
+npx prisma generate;
 cd client && npx quasar build && cd ..;
 
 NODE_ENV=prod nest start;
